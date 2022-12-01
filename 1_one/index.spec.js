ad2fbadb5cbcc299150a1ea9f06114e436bf819b
a-rudolph <adam.rudolph@live.ca>
2022-11-30 23:59:16 -0500

    first commit

---
+++ 1_one/index.spec.js
@@ -0,0 +1,12 @@
+import { theThing } from ".";
+
+const testInput = ``;
+const expected = ``;
+
+describe("first test", () => {
+  it("should pass", () => {
+    const given = theThing(testInput);
+
+    expect(given).toBe(expected);
+  });
+});
