79eb674ec03dbef99210a7ca19c04e4a8c4ac438
a-rudolph <adam.rudolph@live.ca>
2022-12-03 00:10:30 -0500

    init jest

---
+++ src/3_three/index.spec.js
@@ -0,0 +1,7 @@
+const input = require("./index.ts").input;
+
+describe("it works", () => {
+  it("passes", () => {
+    expect(input).toBe("hello");
+  });
+});
---
+++ src/3_three/index.ts
@@ -0,0 +1,5 @@
+const input = "hello";
+
+module.exports = {
+  input,
+};
